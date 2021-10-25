394cf67e3699aad9d79f60444faa6991a33c528a
gael <grenalt3424@gmail.com>
2021-10-25 16:53:24 +0200

    Create HttpClient to communicate with CommandService from PlatformService

--- PlatformService/Startup.cs
+++ PlatformService/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using PlatformService.Data;
+using PlatformService.SyncDataServices.Http;
 
 namespace PlatformService
 {
@@ -32,9 +33,11 @@ namespace PlatformService
                 opt.UseInMemoryDatabase("InMem"));
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
+            
 
             services.AddScoped<IPlatformRepo, PlatformRepo>();
+
+            services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>(); 
 
 
             services.AddControllers();
@@ -42,6 +45,8 @@ namespace PlatformService
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "PlatformService", Version = "v1" });
             });
+
+            Console.WriteLine($"--> CommandService Endpoint : {Configuration["CommandService"]}");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
